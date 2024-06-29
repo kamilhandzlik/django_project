b64192be1d791d9f1b7775bf10f9abba869c03c3
Kamil Handzlik <kamol0209@gmail.com>
2024-06-29 18:08:36 +0200

    My daily pracitce

--- code_for_fun/register/views.py
+++ code_for_fun/register/views.py
@@ -2,11 +2,26 @@ from .forms import RegisterForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
+from django.core.mail import send_mail
+from django.conf import settings
+
 
 class RegisterView(CreateView):
     form_class = RegisterForm
     template_name = 'authentication/register.html'
     success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = form.instance
+        send_mail(
+            'Witamy w CoApp',
+            'Dziękujemy za rejestrację w CoApp.',
+            settings.DEFAULT_FROM_EMAIL,
+            [user.email],
+            fail_silently=False,
+        )
+        return response
 
 class CustomLoginView(LoginView):
     template_name = 'authentication/login.html'
